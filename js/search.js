cee5819cedda059d3c8780722f7f3518f0a84dc6
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-21 01:28:37 +0900

    [FIX] search

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ document.getElementById("searchInput").addEventListener("change", function() {
   var input = document.getElementById("searchInput");
   var filter = input.value.toUpperCase();
 
-  var list = document.getElementsByClassName("emoji");
+  var list = document.getElementsByClassName("emoji-span");
 
   for (var i = 0; i < list.length; i++) {
     var txtValue = list[i].getAttribute("title");
