60b6d6e43673e655789e1300fa96eff5ae966c59
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-26 22:05:19 +0900

    [UPDATE] panel close

--- js/panel.js
+++ js/panel.js
@@ -1,20 +1,33 @@
-//Set initial session storage
-let panelState = sessionStorage.getItem("panel");
-if (panelState == null) {
+whale.tabs.query({ active: true }, function(tabs) {
+  localStorage.setItem("tab", tabs[0].id);
+});
+
+let panelSetting = localStorage.getItem("windowState");
+if (panelSetting == null) {
+  localStorage.setItem("windowState", "popup");
   sessionStorage.setItem("panel", false);
+} else if (panelSetting == "popup") {
+  sessionStorage.setItem("panel", false);
+} else if (panelSetting == "panel") {
+  sessionStorage.setItem("panel", true);
 }
 
 let panelBtn = document.getElementsByClassName("panel");
+if (sessionStorage.getItem("panel") == true) {
+  panelBtn[0].setAttribute("style", "display:none;");
+}
+
 panelBtn[0].addEventListener("click", () => {
   let panelState = sessionStorage.getItem("panel");
   if (panelState == "false") {
     toPanel();
+  } else {
+    toPopup();
   }
 });
 
 function toPanel() {
-  sessionStorage.setItem("panel", true);
-  window.close();
+  localStorage.setItem("windowState", "panel");
 
   whale.windows.create({
     url: "popup/index.html",
@@ -25,4 +38,11 @@ function toPanel() {
     type: "panel",
     focused: true
   });
+
+  window.close();
+}
+
+function toPopup() {
+  localStorage.setItem("windowState", "popup");
+  window.close();
 }
