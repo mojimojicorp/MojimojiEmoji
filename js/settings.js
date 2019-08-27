9495e499a8a9e52b42b63a88f724e577004d7a3b
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-28 01:03:18 +0900

    [ADD] setting buttons

--- js/settings.js
+++ js/settings.js
@@ -8,3 +8,21 @@ settingBtn.addEventListener("click", () => {
   let div = document.querySelector(".setting-container");
   div.setAttribute("style", "display: block;");
 });
+
+let goback = document.querySelector(".goback");
+goback.addEventListener("click", () => {
+  let emojiContainer = document.querySelector(".emoji-container");
+  let footer = document.querySelector(".footer");
+  emojiContainer.setAttribute("style", "display: block;");
+  footer.setAttribute("style", "display: block;");
+
+  let div = document.querySelector(".setting-container");
+  div.setAttribute("style", "display: none;");
+});
+
+let githublink = document.querySelector(".fa-github");
+githublink.addEventListener("click", () => {
+  whale.tabs.create({
+    url: "https://github.com/soyoungjeong/EmojiByEternal"
+  });
+});
