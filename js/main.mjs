24e3c4d43b79fcb31a707789936e0cd882c42a51
Sungdong Jo <sdong001@gmail.com>
2019-09-02 23:07:29 +0900

    Remove duplicate code and Add main module

---
+++ js/main.mjs
@@ -0,0 +1,88 @@
+import Doc from './service/doc.mjs';
+import Cons from './service/const.mjs';
+
+function makeEmojiNav() {
+    const emojiNav = Doc.find('.emoji-nav');
+
+    for (let [ind, name] of Object.entries(Cons.NAV_NAME_LIST)) {
+        const navDiv = Doc.create('div');
+
+        navDiv.className = +ind === 0 ? 'nav-recent' : `nav${ind}`;
+
+        const btnOfLink = Doc.create('a');
+        btnOfLink.className = "btn";
+        btnOfLink.href = `#${name}`;
+
+        const icon = Doc.create('i');
+        icon.className = `fas ${Cons.NAV_ICON_LIST[ind]} fa-2x`;
+        btnOfLink.appendChild(icon);
+
+        navDiv.appendChild(btnOfLink);
+        emojiNav.appendChild(navDiv);
+    }
+}
+
+function makeEmojiList() {
+    const emojiGroup = Doc.find('.emoji-list .groups');
+    /*html*/`
+    <div class="recent_group">
+        <div class="group-title">
+            <a name="recents"><i class="fas fa-history"></i>Recents</a>
+        </div>
+        <div class="emoji-span-container"></div>
+    </div>
+    `
+
+    for (let [ind, name] of Object.entries(Cons.NAV_ICON_LIST)) {
+        const groupDiv = Doc.create('div');
+        groupDiv.className = +ind === 0 ? 'recent_group' : 'group';
+
+        const groupTitle = Doc.create('div');
+        groupTitle.className = 'group-title';
+        groupDiv.appendChild(groupTitle);
+
+        const a = Doc.create('a');
+        a.name = Cons.NAV_NAME_LIST[ind];
+        a.innerHTML = Cons.NAV_TEXT_LIST[ind];
+        groupTitle.appendChild(a);
+
+        const i = Doc.create('i');
+        i.className = `fas ${name}`;
+        a.appendChild(i);
+
+        const emojiContainer = Doc.create('div');
+        emojiContainer.className = 'emoji-span-container';
+        groupDiv.appendChild(emojiContainer);
+
+        emojiGroup.appendChild(groupDiv);
+    }
+}
+
+function includeJS(jsFilePath) {
+    const js = Doc.create('script');
+
+    js.type = "text/javascript";
+    js.src = jsFilePath;
+
+    document.body.appendChild(js);
+}
+
+function afterRendering() {
+    includeJS('../js/copy.js');
+    includeJS('../js/autocopy.js');
+    includeJS('../js/copySetting.js');
+    includeJS('../js/getEmoji.js');
+    includeJS('../js/skintone.js');
+    includeJS('../js/recent.js');
+    includeJS('../js/onecopy.js');
+    includeJS('../js/settings.js');
+    includeJS('../js/search.js');
+    includeJS('../js/panel.js');
+    includeJS('../js/panelSetting.js');
+    includeJS('../js/recentSetting.js');
+    includeJS('../js/sizeSetting.js');
+}
+
+makeEmojiNav();
+makeEmojiList();
+afterRendering();
