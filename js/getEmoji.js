a8ec062c35643bbd64c50461cb07a8accc5c3893
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-11-10 16:35:36 +0900

    [FIX] issue#31

--- js/getEmoji.js
+++ js/getEmoji.js
@@ -1,47 +1,50 @@
-import Doc from './service/doc.mjs';
-import setGrid from './setGrid.mjs';
-import { autocopy, enhancedElements2, clipboard } from './copy/autocopy.mjs';
-import copyEvent from './copy/copyEvent.mjs';
-import renderRecent from './recent/renderRecent.mjs';
+import Doc from "./service/doc.mjs";
+import setGrid from "./setGrid.mjs";
+import { autocopy, enhancedElements2, clipboard } from "./copy/autocopy.mjs";
+import { onecopy } from "./copy/onecopy.mjs";
+import copyEvent from "./copy/copyEvent.mjs";
+import renderRecent from "./recent/renderRecent.mjs";
 
-const sizeSetting = localStorage.getItem('emojiSize');
-const copySetting = localStorage.getItem('copy');
+const sizeSetting = localStorage.getItem("emojiSize");
+const copySetting = localStorage.getItem("copy");
 
-$.getJSON('../emoji.json', async (data) => {
-    // emoji data
-    const EMOJI = data;
+$.getJSON("../emoji.json", async data => {
+  // emoji data
+  const EMOJI = data;
 
-    const groups = Doc.findAll('.emoji-span-container');
+  const groups = Doc.findAll(".emoji-span-container");
 
-    attachEmoji(EMOJI, groups);
-    setSize(groups);
+  attachEmoji(EMOJI, groups);
+  setSize(groups);
 
-    renderRecent();
+  renderRecent();
 
-    // 각 emoji마다 eventlistener 추가 & copy 버튼, reset 버튼 eventlistener 추가
-    copyEvent();
+  // 각 emoji마다 eventlistener 추가 & copy 버튼, reset 버튼 eventlistener 추가
+  copyEvent();
 
-    if (copySetting == 'auto') {
-        autocopy();
-    }
+  if (copySetting == "auto") {
+    autocopy();
+  } else if (copySetting == "manual") {
+    onecopy();
+  }
 });
 
 function setSize() {
-    switch (sizeSetting) {
-    case 'small':
-        setGrid(15);
-        break;
-    case 'normal':
-        setGrid(10);
-        break;
-    case 'big':
-        setGrid(5);
-        break;
-    }
+  switch (sizeSetting) {
+    case "small":
+      setGrid(15);
+      break;
+    case "normal":
+      setGrid(10);
+      break;
+    case "big":
+      setGrid(5);
+      break;
+  }
 }
 
 function attachEmoji(emoji, groups) {
-    emoji.forEach((data) => {
+  emoji.forEach(data => {
     let span = Doc.create("span");
     span.setAttribute("class", "emoji-span");
     span.setAttribute("title", data.name);
