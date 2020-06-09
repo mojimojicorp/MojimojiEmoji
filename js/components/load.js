81c4147f6f75d7009677968a10b2b27745a2b01e
SeWonKim <swon962@gmail.com>
2020-06-09 18:13:24 +0900

    [Refact] #18 index.html 구조 변경
    header와 emoji-list, copyStatus 를 모듈화하여 index.html이 간결해지도록
    만듦.

---
+++ js/components/load.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/extensions */
+import makeHeader from './Header/index.mjs';
+import makeCopyStatus from './CopyStatus/index.mjs';
+
+makeHeader();
+
+makeCopyStatus();
--- js/main.mjs
+++ js/main.mjs
@@ -1,25 +1,26 @@
-import Doc from "./service/doc.mjs";
-import Cons from "./service/const.mjs";
-import Util from "./service/util.mjs";
+/* eslint-disable import/extensions */
+import Doc from './service/doc.mjs';
+import Cons from './service/const.mjs';
+import Util from './service/util.mjs';
 
-import firstSet from "./settings/local.mjs";
-import setCopyBtn from "./settings/copySetting.js";
-import setSizeBtn from "./settings/sizeSetting.js";
-import setRecentBtn from "./settings/recentSetting.js";
+import firstSet from './settings/local.mjs';
+import setCopyBtn from './settings/copySetting.js';
+import setSizeBtn from './settings/sizeSetting.js';
+import setRecentBtn from './settings/recentSetting.js';
 
 function makeEmojiNav() {
-  const emojiNav = Doc.find(".emoji-nav");
+  const emojiNav = Doc.find('.emoji-nav');
 
-  for (let [ind, name] of Object.entries(Cons.NAV_NAME_LIST)) {
-    const navDiv = Doc.create("div");
+  for (const [ind, name] of Object.entries(Cons.NAV_NAME_LIST)) {
+    const navDiv = Doc.create('div');
 
-    navDiv.className = +ind === 0 ? "nav-recent" : `nav${ind}`;
+    navDiv.className = +ind === 0 ? 'nav-recent' : `nav${ind}`;
 
-    const btnOfLink = Doc.create("a");
-    btnOfLink.className = "btn";
+    const btnOfLink = Doc.create('a');
+    btnOfLink.className = 'btn';
     btnOfLink.href = `#${name}`;
 
-    const icon = Doc.create("i");
+    const icon = Doc.create('i');
     icon.className = `fas ${Cons.NAV_ICON_LIST[ind]} fa-2x`;
     btnOfLink.appendChild(icon);
 
@@ -29,8 +30,8 @@ function makeEmojiNav() {
 }
 
 function makeEmojiList() {
-  const emojiGroup = Doc.find(".emoji-list .groups");
-  /*html*/ `
+  const emojiGroup = Doc.find('.emoji-list .groups');
+  /* html */ `
     <div class="recent_group">
         <div class="group-title">
             <a name="recents"><i class="fas fa-history"></i>Recents</a>
@@ -39,25 +40,25 @@ function makeEmojiList() {
     </div>
     `;
 
-  for (let [ind, name] of Object.entries(Cons.NAV_ICON_LIST)) {
-    const groupDiv = Doc.create("div");
-    groupDiv.className = +ind === 0 ? "recent_group" : "group";
+  for (const [ind, name] of Object.entries(Cons.NAV_ICON_LIST)) {
+    const groupDiv = Doc.create('div');
+    groupDiv.className = +ind === 0 ? 'recent_group' : 'group';
 
-    const groupTitle = Doc.create("div");
-    groupTitle.className = "group-title";
+    const groupTitle = Doc.create('div');
+    groupTitle.className = 'group-title';
     groupDiv.appendChild(groupTitle);
 
-    const a = Doc.create("a");
+    const a = Doc.create('a');
     a.name = Cons.NAV_NAME_LIST[ind];
     a.innerHTML = Cons.NAV_TEXT_LIST[ind];
     groupTitle.appendChild(a);
 
-    const i = Doc.create("i");
+    const i = Doc.create('i');
     i.className = `fas ${name}`;
     a.appendChild(i);
 
-    const emojiContainer = Doc.create("div");
-    emojiContainer.className = "emoji-span-container";
+    const emojiContainer = Doc.create('div');
+    emojiContainer.className = 'emoji-span-container';
     groupDiv.appendChild(emojiContainer);
 
     emojiGroup.appendChild(groupDiv);
@@ -65,14 +66,14 @@ function makeEmojiList() {
 }
 
 function afterRendering() {
-  Util.includeJS("../js/settings/settings.js");
+  Util.includeJS('../js/settings/settings.js');
 
-  Util.includeJS("../js/getEmoji.js");
-  Util.includeJS("../js/skintone.js");
+  Util.includeJS('../js/getEmoji.js');
+  Util.includeJS('../js/skintone.js');
 
-  Util.includeJS("../js/copy.js");
-  Util.includeJS("../js/search.js");
-  Util.includeJS("../js/panel.js");
+  Util.includeJS('../js/copy.js');
+  Util.includeJS('../js/search.js');
+  Util.includeJS('../js/panel.js');
 }
 
 function setSetting() {
