0d730d64b1ee1c665116acd1fcec6861702ac6cd
SeWonKim <swon962@gmail.com>
2020-06-09 17:41:02 +0900

    [Chore] eslint 적용

--- js/service/removeDuplicate.mjs
+++ js/service/removeDuplicate.mjs
@@ -1,22 +1,20 @@
 function removeDuplicate(arr) {
-  var tempArr = [];
+  const tempArr = [];
 
-  for (var i = 0; i < arr.length; i++) {
-    if (tempArr.length == 0) {
+  for (let i = 0; i < arr.length; i++) {
+    if (tempArr.length === 0) {
       tempArr.push(arr[i]);
     } else {
-      var duplicatesFlag = true;
-      for (var j = 0; j < tempArr.length; j++) {
+      let duplicatesFlag = true;
+      for (let j = 0; j < tempArr.length; j++) {
         if (arr[i].char == null) {
-          if (tempArr[j] == arr[i]) {
+          if (tempArr[j] === arr[i]) {
             duplicatesFlag = false;
             break;
           }
-        } else {
-          if (tempArr[j].char == arr[i].char) {
-            duplicatesFlag = false;
-            break;
-          }
+        } else if (tempArr[j].char === arr[i].char) {
+          duplicatesFlag = false;
+          break;
         }
       }
       if (duplicatesFlag) {
--- js/service/util.mjs
+++ js/service/util.mjs
@@ -1,13 +1,13 @@
 const Util = {
-  includeJS: jsFilePath => {
-    const js = document.createElement("script");
+  includeJS: (jsFilePath) => {
+    const js = document.createElement('script');
 
-    //js.type = 'text/javascript';
-    js.type = "module";
+    // js.type = 'text/javascript';
+    js.type = 'module';
     js.src = jsFilePath;
 
     document.body.appendChild(js);
-  }
+  },
 };
 
 export default Util;
