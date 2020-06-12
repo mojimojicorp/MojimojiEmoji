1e8ba3dfc33f9dd7a47d01e5740c47a0f2349c7a
soyoungjeong <hana46798@gmail.com>
2020-06-12 22:53:39 +0900

    [Refact] #18 로딩 속도 개선
    json 파일을 바로 import하여 데이터 통신 과정을 줄임.
    appendchild가 너무 많이 일어나서 속도가 느려지는 것을 없애기 위해
    apeendchild하는 부분을 하나로 묶어 innerHTML로 변경.

--- js/emoji/attachEmoji.mjs
+++ js/emoji/attachEmoji.mjs
@@ -5,66 +5,75 @@ import { renderEmoji } from './renderEmoji.mjs';
 
 const groups = Doc.findAll('.emoji-span-container');
 
-const attach = (category, emoji) => {
-  switch (category) {
-    case 'people':
-      groups[1].appendChild(emoji);
-      break;
-    case 'nature':
-      groups[2].appendChild(emoji);
-      break;
-    case 'foodAndDrink':
-      groups[3].appendChild(emoji);
-      break;
-    case 'activity':
-      groups[4].appendChild(emoji);
-      break;
-    case 'place':
-      groups[5].appendChild(emoji);
-      break;
-    case 'objects':
-      groups[6].appendChild(emoji);
-      break;
-    case 'symbols':
-      groups[7].appendChild(emoji);
-      break;
-    case 'flags':
-      groups[8].appendChild(emoji);
-      break;
-  }
+const attach = (emoji) => {
+  const categories = Object.keys(emoji);
+  categories.forEach((category) => {
+    switch (category) {
+      case 'people':
+        groups[1].innerHTML = emoji[category];
+        break;
+      case 'nature':
+        groups[2].innerHTML = emoji[category];
+        break;
+      case 'foodAndDrink':
+        groups[3].innerHTML = emoji[category];
+        break;
+      case 'activity':
+        groups[4].innerHTML = emoji[category];
+        break;
+      case 'place':
+        groups[5].innerHTML = emoji[category];
+        break;
+      case 'objects':
+        groups[6].innerHTML = emoji[category];
+        break;
+      case 'symbols':
+        groups[7].innerHTML = emoji[category];
+        break;
+      case 'flags':
+        groups[8].innerHTML = emoji[category];
+        break;
+    }
+  });
 };
 
-// 각 emoji를 container에 붙이기
 const attachEmoji = (emoji) => {
-  emoji.forEach((data) => {
-    const emojiItem = Doc.create('div');
-    emojiItem.className = 'emoji-span';
-    emojiItem.title = data.name;
-
-    // emoji에 맞는 image로 렌더링
-    const emoji = renderEmoji(data);
-
-    // 초기설정: skintone을 포함한 이모티콘 숨기기
-    if (data.codes.includes(Cons.SKINTONE_COLOR_LIST[0])) {
-      emojiItem.className = 'emoji-span skin-tone light-skin-tone';
-      emojiItem.style.display = 'none';
-    } else if (data.codes.includes(Cons.SKINTONE_COLOR_LIST[1])) {
-      emojiItem.className = 'emoji-span skin-tone medium-light-skin-tone';
-      emojiItem.style.display = 'none';
-    } else if (data.codes.includes(Cons.SKINTONE_COLOR_LIST[2])) {
-      emojiItem.className = 'emoji-span skin-tone medium-skin-tone';
-      emojiItem.style.display = 'none';
-    } else if (data.codes.includes(Cons.SKINTONE_COLOR_LIST[3])) {
-      emojiItem.className = 'emoji-span skin-tone medium-dark-skin-tone';
-      emojiItem.style.display = 'none';
-    } else if (data.codes.includes(Cons.SKINTONE_COLOR_LIST[4])) {
-      emojiItem.className = 'emoji-span skin-tone dark-skin-tone';
-      emojiItem.style.display = 'none';
+  const obj = emoji.reduce((acc, cur) => {
+    let skinToneClassName = '';
+    let skinToneStyle = '';
+    if (cur.codes.includes(Cons.SKINTONE_COLOR_LIST[0])) {
+      skinToneClassName = 'skin-tone light-skin-tone';
+      skinToneStyle = "style = 'display:none;'";
+    } else if (cur.codes.includes(Cons.SKINTONE_COLOR_LIST[1])) {
+      skinToneClassName = 'skin-tone medium-light-skin-tone';
+      skinToneStyle = "style = 'display:none;'";
+    } else if (cur.codes.includes(Cons.SKINTONE_COLOR_LIST[2])) {
+      skinToneClassName = 'skin-tone medium-skin-tone';
+      skinToneStyle = "style = 'display:none;'";
+    } else if (cur.codes.includes(Cons.SKINTONE_COLOR_LIST[3])) {
+      skinToneClassName = 'skin-tone medium-dark-skin-tone';
+      skinToneStyle = "style = 'display:none;'";
+    } else if (cur.codes.includes(Cons.SKINTONE_COLOR_LIST[4])) {
+      skinToneClassName = 'skin-tone dark-skin-tone';
+      skinToneStyle = "style = 'display:none;'";
     }
 
-    emojiItem.appendChild(emoji);
-    attach(data.category, emojiItem);
-  });
+    if (acc.hasOwnProperty(cur.category)) {
+      acc[
+        cur.category
+      ] += `<div class="emoji-span ${skinToneClassName}" title="${
+        cur.name
+      }" ${skinToneStyle}>${renderEmoji(cur)}</div>`;
+      return acc;
+    }
+
+    acc[cur.category] = `<div class="emoji-span" title="${
+      cur.name
+    }">${renderEmoji(cur)}</div>`;
+    return acc;
+  }, {});
+
+  attach(obj);
 };
 
 export { attachEmoji };
