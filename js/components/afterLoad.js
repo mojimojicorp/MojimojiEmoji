e124dcdc6cae6ff9765909547c9402eb0b011715
SeWonKim <swon962@gmail.com>
2020-06-16 21:09:11 +0900

    [Chore] merge 후 작동하지 않는 기능 동작하도록 수정
    - Header의 panel, setting 동작하도록 수정
    - 기존 panel 코드의 jquery 제거
    - search 기능 동작하도록 수정

--- js/components/afterLoad.js
+++ js/components/afterLoad.js
@@ -1,14 +1,18 @@
 /* eslint-disable import/extensions */
 import Util from '../service/util.mjs';
 import setSetting from '../settings/setSetting.js';
+import handleSetting from '../settings/handelSetting.js';
+import firstSet from '../settings/firstSetting.js';
 
 function afterRendering() {
-  // setting 버튼 설정
-  setSetting();
   // emoji 불러오기
   Util.includeJS('../js/emoji/getEmoji.js');
-  // Util.includeJS('../js/copy.js');
-  // Util.includeJS('../js/search.js');
+  Util.includeJS('../js/panel.js');
+  Util.includeJS('../js/search.js');
+
+  handleSetting(); // event listener 등록
+  firstSet(); // 초기 storage 설정
+  setSetting(); // setting 버튼 설정
 }
 
 afterRendering();
--- js/copy/setCopyEvent.mjs
+++ js/copy/setCopyEvent.mjs
@@ -1,6 +1,8 @@
 import Doc from '../service/doc.mjs';
+import { copy } from './copy.mjs';
 
 function setCopyEvent() {
+  // emoji 버튼 클릭 이번트
   const emojis = Doc.findAll('.emoji-button');
 
   emojis.forEach((emoji) => {
@@ -10,6 +12,20 @@ function setCopyEvent() {
       group.value = content.concat(emoji.value);
     });
   });
+
+  // copy 버튼 클릭 이벤트
+  const CopyBtn = Doc.find('#copy_btn');
+  CopyBtn.addEventListener('click', copy);
+
+  // reset 버튼 클릭 이벤트
+  const resetBtn = Doc.find('#reset_btn');
+  resetBtn.addEventListener(
+    'click',
+    () => {
+      Doc.find('#copy_group').value = '';
+    },
+    false
+  );
 }
 
 export { setCopyEvent };
