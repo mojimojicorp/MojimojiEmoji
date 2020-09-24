13ddfb8bf2535ad14c736209430b03ee1aeb0436
soyoungjeong <hana46798@gmail.com>
2020-09-24 23:20:08 +0900

    [Fix] skintone button 버그 수정

--- js/events/buttonEvent/skintone.js
+++ js/events/buttonEvent/skintone.js
@@ -12,8 +12,9 @@ export default function addSkintoneButtonEvent() {
     const color = e.target.parentNode.classList[1];
 
     emojiSpans.forEach((emoji) => {
-      if (!emoji.title.includes(':')) return;
-      if (emoji.title.includes(color)) {
+      const separator = emoji.title.indexOf(':');
+      if (separator === -1) return;
+      if (emoji.title.slice(separator) === `: ${color}`) {
         emoji.style.display = '';
       } else emoji.style.display = 'none';
     });
