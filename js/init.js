5418a61d489983d613554ccef1902571a6988dcb
SeWon Kim <swon962@gmail.com>
2020-10-02 13:39:33 +0900

    chore: localStorage의 firstLoad 값 확인하는 조건문 수정

--- js/init.js
+++ js/init.js
@@ -48,9 +48,8 @@ function setSettingButtonsCss() {
 
 export default function initialSetting() {
   // 프로그램 첫 로드 시 Localstorage setting
-  if (localStorage.getItem('firstLoad') === null) {
-    localStorage.setItem('firstLoad', false);
-
+  if (!localStorage.getItem('firstLoad')) {
+    localStorage.setItem('firstLoad', true);
     localStorage.setItem('copy', 'auto');
     localStorage.setItem('emojiSize', 'normal');
     localStorage.setItem('recentNum', 20);
