3e5bc0b4525b75686887629a61a9fb4e4ae9e233
SeWon Kim <swon962@gmail.com>
2020-12-28 17:02:37 +0900

    fix: search 안되는 버그 수정

--- js/events/inputEvent/search.js
+++ js/events/inputEvent/search.js
@@ -2,11 +2,11 @@
 import Doc from '../../utils/doc.js';
 
 export default function addSearchInputEvent() {
-  const emojiElements = Doc.findAll('.emoji-span');
-
+  
   Doc.find('#searchInput').addEventListener('change', (e) => {
     const searchInput = e.target.value.toUpperCase();
-
+    
+    const emojiElements = Doc.findAll('.emoji-span');
     emojiElements.forEach((emoji) => {
       const title = emoji.getAttribute('title');
       if (title.toUpperCase().includes(searchInput)) {
